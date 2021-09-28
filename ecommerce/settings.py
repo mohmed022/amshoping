5e4e80260e84ac9ec0c3c1140a094645b408971e
mohmed022 <aml148200@gemail.com>
2021-09-28 13:43:51 -0700

    klkl

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = "django-insecure-*-7nxx+dkrjm*oyaxt47l#jwog-i#z9bwcyr@g&g^fc*e)nx-_
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'amshoping.herokuapp.com']
+ALLOWED_HOSTS = ['amshoping.herokuapp.com']
 
 
 # Application definition
@@ -74,13 +74,15 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     "default": {
@@ -96,16 +98,16 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "database-1.cluster-ccs9eitlywtm.ap-south-1.rds.amazonaws.com",
-        "USER": "admin",
-        "PASSWORD": "Aml782000",
-        "HOST": "ec2-18-209-143-227.compute-1.amazonaws.com",
-        "PORT": "3306",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "database-1.cluster-ccs9eitlywtm.ap-south-1.rds.amazonaws.com",
+#         "USER": "admin",
+#         "PASSWORD": "Aml782000",
+#         "HOST": "ec2-18-209-143-227.compute-1.amazonaws.com",
+#         "PORT": "3306",
+#     }
+# }
 
 
 
@@ -149,16 +151,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+# STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 #MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 # Basket session ID
 BASKET_SESSION_ID = "basket"
@@ -176,3 +178,21 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static")
+
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+#STATIC_ROOT = "/home/cfedeploy/webapps/cfehome_static_root/"
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media/")
