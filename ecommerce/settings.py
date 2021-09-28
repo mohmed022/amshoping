b6ecde8fc417ce8d231d2388717be589c92c65d3
mohmed022 <aml148200@gemail.com>
2021-09-28 14:42:56 -0700

    تا

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,8 +81,8 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
 #     "default": {
