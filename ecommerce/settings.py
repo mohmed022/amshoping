6da28fe591a2a2651c67f0133dffbdba80a54501
mohmed022 <aml148200@gemail.com>
2021-09-27 23:27:43 -0700

    first commi45555lkjghgkkjt

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,12 +129,13 @@ USE_TZ = True
 
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfile")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path 
+from django.urls import re_path
+from django.views.static import serve
 
 # from django.conf import settings
 # from django.urls import re_path
@@ -15,23 +17,22 @@ urlpatterns = [
     path("account/", include("ecommerce.apps.account.urls", namespace="account")),
     path("orders/", include("ecommerce.apps.orders.urls", namespace="orders")),
 ]
-# from django.urls import re_path
-# from django.views.static import serve
+
 
 # ... the rest of your URLconf goes here ...
 
-# if settings.DEBUG:
-#     urlpatterns += [
-#         re_path(r'^media/(?P<path>.*)$', serve, {
-#             'document_root': settings.MEDIA_ROOT,
-#         }),
-#     ]
+if settings.DEBUG:
+    urlpatterns += [
+         re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
    
-    # # Added Following Two Lines Of Code
+    # Added Following Two Lines Of Code
     # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
