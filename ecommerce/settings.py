3fee1279d70f82804aaf1271751cdf88e969d64f
mohmed022 <aml148200@gemail.com>
2021-09-28 00:22:52 -0700

    first commi45555lkjghgkk6556d555y5jt

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -74,23 +74,38 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 
 # DATABASES = {
 #     "default": {
 #         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": "dktvc44r6mj8h",
-#         "USER": "tgntvalztciipm",
-#         "PASSWORD": "85d03aee702c5edb29f9a63de531e09a6e491ea4ac46d06e680656a0a05d5cfa",
-#         "HOST": "ec2-18-209-143-227.compute-1.amazonaws.com",
+#         "NAME": "amshoping",
+#         "USER": "postgres",
+#         "PASSWORD": "aml",
+#         "HOST": "localhost",
 #         "PORT": "5432",
 #     }
 # }
+
+
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "dktvc44r6mj8h",
+        "USER": "tgntvalztciipm",
+        "PASSWORD": "85d03aee702c5edb29f9a63de531e09a6e491ea4ac46d06e680656a0a05d5cfa",
+        "HOST": "ec2-18-209-143-227.compute-1.amazonaws.com",
+        "PORT": "5432",
+    }
+}
 
 
 
